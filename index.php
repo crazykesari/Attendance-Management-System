e751649d3dc1e827b0cdbf7c3be2062e5bda4cf9
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2020-04-06 14:01:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<title>Attendance Management System</title>
+<head>
+<link rel="stylesheet"type="text/css"href="stylesheet/navigation.css">
+<link rel="stylesheet"type="text/css"href="stylesheet/index.css">
+</head>
+<body>
+<div id="outer">
+
+<div id="navigation"> </br>
+     <h1><b>Online Attendance Management System</b></h1>
+      
+         </div></br>
+      <div id="tool"	></br>
+	  <div id="box"style="margin-left:1000px">
+
+	   </div>
+         </div>
+		 
+		 <div id="images">
+		 
+		 <a href="login.php"><div class="block"id="child1">
+		 <img class="link"src="images/admin.png">
+		  <h1><i>Administrative</i></h1>
+		    </div></a>
+   		 <a href="login.php"><div class="block"id="child2">
+		  <img class="link"src="images/teach.jpg"height="250px">
+		  <h1><i>Teacher</i></h1>
+		    </div></a>
+
+		 <a href="login.php"><div class="block"id="child3">
+		 <img class="link"src="images/student.png">
+		  
+		    </div></a>
+		 		 
+		 
+		 
+		    </div>
+		 
+		 
+		 
+		 
+		 
+</div>
+</body>
+</html>
