b494f1e6f75a174428720210c4552303a4c46b3b
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2020-04-14 18:27:52 +0530

    Add files via upload

---
+++ faculty/lastlogout.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+if(session_destroy())
+{
+}
+?>
+
+
+<html>
+<title>Faculty Page</title>
+<head>
+<link rel="stylesheet"type="text/css"href="stylesheet/twobar.css">
+</head>
+<body bgcolor="powderblue">
+<div id="outer"></br>
+   <div id="navigation"> </br>
+     <h1><b>Online Attendance Management System</b></h1>
+      
+         </div></br>
+      <div id="tool"></br>
+	  <div id="box">
+       <ul>
+
+	   <li><a class="attendance"href="index.php">Home</a></li>
+	   <li><a class="login"href="login.php">Login</a></li>
+
+	   
+	   </ul>
+	   </div>
+         </div></br></br>
+		 
+		 <div id="content">
+		 
+         <h1>Attendance succesfully saved!!</h1></br>
+
+		
+             </div>
+
+
+
+
+
+
+
+
+</div>
+</body>
+</html>
