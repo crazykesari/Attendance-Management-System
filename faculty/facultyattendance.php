a8f836e0a322f4f9b1a39eed07c8bc05d5282c2a
Abhishek kesarwani <47149427+crazykesari@users.noreply.github.com>
2020-04-14 18:29:37 +0530

    Add files via upload

---
+++ faculty/facultyattendance.php
@@ -0,0 +1,41 @@
+<?php
+include('session.php');
+?>
+<html>
+<title>Attendance Page</title>
+<head>
+<link rel="stylesheet"type="text/css"href="stylesheet/homepage.css">
+</head>
+<body bgcolor="powderblue">
+<div id="outer"></br>
+   <div id="navigation"> </br>
+     <h1><b>Online Attendance Management System</b></h1>
+      
+         </div></br>
+      <div id="tool"></br>
+	  <div id="box">
+       <ul>
+	   <li><a class="home" href="#">Home</a></li>
+	   <li><a class="attendance"href="#">Attendance</a></li>
+	   <li><a class="notes"href="#">Notes</a></li>
+	   <li><a class="logout"href="logout.php">Logout</a></li>
+	   
+	   </ul>
+	   </div>
+         </div>
+		 </br></br>
+		 <div id="block">
+		 <div id="view">
+		<ul>
+	   <li><a class="takeattendance" href="attendancedoor.php">Take Attendance</a></li>
+	   <li><a class="view"href="#">View Attendance</a></li>
+	   <li><a class="viewstudent"href="#">View Student</a></li>
+	   
+	   </ul>
+	    
+		     </div>		 
+			  			  
+		 </div>
+</div>
+</body>
+</html>
